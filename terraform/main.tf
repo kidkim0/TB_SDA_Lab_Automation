853c99c8f2cadc8f045f77c4e76312b4b64d666e
kidkim0 <kidong25019@gmail.com>
2025-10-28 14:32:35 +0000

    make environment varibles

--- terraform/main.tf
+++ terraform/main.tf
@@ -8,4 +8,22 @@ terraform {
 }
 
 provider "cml2" {
+  # declare environment varibales
+  # eg. export TF_VAR_CML_ADDRESS=https://192.0.0.1
+  address = var.CML_ADDRESS
+  username = var.CML_USERNAME
+  password = var.CML_PASSWORD
+  skip_verify = true
+}
+
+variable "CML_ADDRESS" {
+  type = string
+}
+
+variable "CML_USERNAME" {
+  type = string
+}
+
+variable "CML_PASSWORD" {
+  type = string
 }
