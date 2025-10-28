10c2ed3e8288a1eec03c7792fbe2f4df43565a6d
kidkim0 <kidong25019@gmail.com>
2025-10-28 15:32:31 +0000

    Add startup config

--- terraform/lab.tf
+++ terraform/lab.tf
@@ -6,15 +6,18 @@ resource "cml2_node" "sw1" {
   lab_id = cml2_lab.testlab.id
   label  = "sw1"
   nodedefinition = "cat9000v-uadp"
-  x = 100
-  y = 100
+  x = 200
+  y = 0
+  configuration = <<-EOT
+      hostname sw1
+  EOT
 }
 
 resource "cml2_node" "sw2" {
   lab_id = cml2_lab.testlab.id
   label  = "sw2"
   nodedefinition = "cat9000v-uadp"
-  x = 100
+  x = 200
   y = 200
 }
 
@@ -22,8 +25,8 @@ resource "cml2_node" "sw3" {
   lab_id = cml2_lab.testlab.id
   label  = "sw3"
   nodedefinition = "cat9000v-uadp"
-  x = 100
-  y = 300
+  x = 200
+  y = 400
 }
 
 resource "cml2_node" "ext" {
@@ -32,7 +35,7 @@ resource "cml2_node" "ext" {
   nodedefinition = "external_connector"
   configuration = "System Bridge"
   x = 0
-  y = 100
+  y = 0
 }
 
 resource "cml2_node" "unmanaged" {
